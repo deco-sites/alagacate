d5e9830d8681a0a95a045359ccffaf3f73815342
lui-dias <github.anyplace863@passinbox.com>
2025-02-24 11:28:35 -0300

    update decopilot workflow

--- .github/scripts/main.py
+++ .github/scripts/main.py
@@ -29,69 +29,69 @@ from tokenizers import Tokenizer
 print(11)
 from ujson import loads
 
-# print('a')
-# class TSXCodeTextSplitter(RecursiveCharacterTextSplitter):
-#     def __init__(self, **kwargs):
-#         html_separators = self.get_separators_for_language(Language.HTML)
-#         ts_separators = self.get_separators_for_language(Language.TS)
+print('a')
+class TSXCodeTextSplitter(RecursiveCharacterTextSplitter):
+    def __init__(self, **kwargs):
+        html_separators = self.get_separators_for_language(Language.HTML)
+        ts_separators = self.get_separators_for_language(Language.TS)
 
-#         super().__init__(separators=html_separators + ts_separators, **kwargs)
-
-
-# ts_splitter = RecursiveCharacterTextSplitter(
-#     chunk_size=1000,
-#     chunk_overlap=200,
-#     separators=RecursiveCharacterTextSplitter.get_separators_for_language(Language.TS),
-# )
-# tsx_splitter = TSXCodeTextSplitter(chunk_size=1000, chunk_overlap=200)
-# json_splitter = RecursiveJsonSplitter()
-# generic_text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
-# cuid = Cuid()
-
-# splitter_by_extension: dict[str, TextSplitter] = {
-#     ".tsx": tsx_splitter.split_text,
-#     ".ts": ts_splitter.split_text,
-#     ".json": partial(json_splitter.split_text, convert_lists=True),
-#     ".css": generic_text_splitter.split_text,
-# }
-
-# extensions = list(splitter_by_extension.keys())
-# exclude_dirs = {
-#     "node_modules",
-#     "public",
-#     "static",
-#     ".github",
-# }
-
-# REPO_NAME = getenv("REPO_NAME")
-# ALL_CHANGED_FILES = getenv("ALL_CHANGED_FILES")
-# MISTRAL_API_KEY = getenv("MISTRAL_API_KEY")
-# TURBOPUFFER_API_KEY = getenv("TURBOPUFFER_API_KEY")
-
-# if not REPO_NAME:
-#     raise ValueError("REPO_NAME is not set")
-
-# if not ALL_CHANGED_FILES:
-#     raise ValueError("ALL_CHANGED_FILES is not set")
-
-# if not MISTRAL_API_KEY:
-#     raise ValueError("MISTRAL_API_KEY is not set")
-
-# if not TURBOPUFFER_API_KEY:
-#     raise ValueError("TURBOPUFFER_API_KEY is not set")
-
-# embeddings = MistralAIEmbeddings(
-#     tokenizer=Tokenizer.from_pretrained("model-hub/Mixtral-8x7B-v0.1"),
-#     max_retries=10,
-#     wait_time=5,
-# )
-
-# tp.api_key = getenv("TURBOPUFFER_API_KEY")
-# tp.api_base_url = "https://gcp-us-east4.turbopuffer.com"
+        super().__init__(separators=html_separators + ts_separators, **kwargs)
 
 
-# def format_namespace(name: str):
-#     return f'site-{name}'
+ts_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=1000,
+    chunk_overlap=200,
+    separators=RecursiveCharacterTextSplitter.get_separators_for_language(Language.TS),
+)
+tsx_splitter = TSXCodeTextSplitter(chunk_size=1000, chunk_overlap=200)
+json_splitter = RecursiveJsonSplitter()
+generic_text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+cuid = Cuid()
+
+splitter_by_extension: dict[str, TextSplitter] = {
+    ".tsx": tsx_splitter.split_text,
+    ".ts": ts_splitter.split_text,
+    ".json": partial(json_splitter.split_text, convert_lists=True),
+    ".css": generic_text_splitter.split_text,
+}
+
+extensions = list(splitter_by_extension.keys())
+exclude_dirs = {
+    "node_modules",
+    "public",
+    "static",
+    ".github",
+}
+
+REPO_NAME = getenv("REPO_NAME")
+ALL_CHANGED_FILES = getenv("ALL_CHANGED_FILES")
+MISTRAL_API_KEY = getenv("MISTRAL_API_KEY")
+TURBOPUFFER_API_KEY = getenv("TURBOPUFFER_API_KEY")
+
+if not REPO_NAME:
+    raise ValueError("REPO_NAME is not set")
+
+if not ALL_CHANGED_FILES:
+    raise ValueError("ALL_CHANGED_FILES is not set")
+
+if not MISTRAL_API_KEY:
+    raise ValueError("MISTRAL_API_KEY is not set")
+
+if not TURBOPUFFER_API_KEY:
+    raise ValueError("TURBOPUFFER_API_KEY is not set")
+
+embeddings = MistralAIEmbeddings(
+    tokenizer=Tokenizer.from_pretrained("model-hub/Mixtral-8x7B-v0.1"),
+    max_retries=10,
+    wait_time=5,
+)
+
+tp.api_key = getenv("TURBOPUFFER_API_KEY")
+tp.api_base_url = "https://gcp-us-east4.turbopuffer.com"
+
+
+def format_namespace(name: str):
+    return f'site-{name}'
 
 
 # ns = tp.Namespace(format_namespace(REPO_NAME))
