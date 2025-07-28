fdbae92064994016e72b240db7af543216252474
Luigi <66072698+lui-dias@users.noreply.github.com>
2025-07-28 14:12:40 -0300

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 /// <reference no-default-lib="true"/>
 /// <reference lib="dom" />
-/// <reference lib="deno.ns" />
+/// <reference lib="deno.ns" /> 
 /// <reference lib="esnext" />
 
 import { start } from "$fresh/server.ts";
