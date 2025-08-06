416db272afe86fb9c627f4072a05c8c7769b4545
Luigi <luigi@carcara.tech>
2025-08-06 18:07:13 -0300

    Revert to fdbae92064994016e72b240db7af543216252474

--- main.ts
+++ main.ts
@@ -1,10 +1,22 @@
 /// <reference no-default-lib="true"/>
 /// <reference lib="dom" />
-/// <reference lib="deno.ns" /> 
+/// <reference lib="deno.ns" />
 /// <reference lib="esnext" />
 
 import { start } from "$fresh/server.ts";
 import config from "./fresh.config.ts";
 import manifest from "./fresh.gen.ts";
+
+
+  ______________
+< miaaaaaaaaaauu >
+  --------------
+         \   ^__^ 
+          \  (oo)\_______
+             (__)\       )\/\\
+                 ||----w |
+                 ||     ||
+    
+
 
 await start(manifest, config);
