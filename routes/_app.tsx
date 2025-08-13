58fa125c59bd155ebc2196d91ed91835ab1cb24a
lui-dias <github.anyplace863@passinbox.com>
2025-08-13 20:42:34 -0300

    testing boom

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -2,12 +2,24 @@ import { asset, Head } from "$fresh/runtime.ts";
 import { defineApp } from "$fresh/server.ts";
 import { useScript } from "@deco/deco/hooks";
 import { Context } from "@deco/deco";
+
+let INCOMING_EXPLOSION: number[] = [];
+
 const serviceWorkerScript = () =>
   addEventListener("load", () =>
     navigator && navigator.serviceWorker &&
     navigator.serviceWorker.register("/sw.js"));
 export default defineApp(async (_req, ctx) => {
   const revision = await Context.active().release?.revision();
+
+  if (_req.url.includes("explosion")) {
+    INCOMING_EXPLOSION = [
+      ...INCOMING_EXPLOSION,
+      ...Array.from(new Array(25_000_000).keys()),
+    ];
+    console.log("aaaaaaaaaaaaaaaaaaaaaaaaaaa");
+    console.log(INCOMING_EXPLOSION.length);
+  }
   return (
     <>
       {/* Include Icons and manifest */}
